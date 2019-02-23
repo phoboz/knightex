6fdbd840bb3f4a8b2f6dc29891a236cec7241f3f
phoboz <phoboz@vmx.nu>
2019-02-23 23:29:34 +0100

    Reduce hight of standing knight

--- source/knight.c
+++ source/knight.c
@@ -29,14 +29,14 @@ const signed char knight_1[]=
 };
 const signed char knight_2[]=
 {	(signed char) +0, +11*BLOW_UP, +6*BLOW_UP, // sync and move to y, x
-	(signed char) +0, +10*BLOW_UP, +0*BLOW_UP, // additional sync move to y, x
+	(signed char) +0, +7*BLOW_UP, +0*BLOW_UP, // additional sync move to y, x
 	(signed char) -1, -8*BLOW_UP, -5*BLOW_UP, // draw, y, x
 	(signed char) -1, +4*BLOW_UP, -1*BLOW_UP, // draw, y, x
 	(signed char) -1, -4*BLOW_UP, -2*BLOW_UP, // draw, y, x
 	(signed char) -1, -3*BLOW_UP, +1*BLOW_UP, // draw, y, x
-	(signed char) -1, -10*BLOW_UP, -1*BLOW_UP, // draw, y, x
+	(signed char) -1, -7*BLOW_UP, -1*BLOW_UP, // draw, y, x
 	(signed char) -1, +0*BLOW_UP, +4*BLOW_UP, // draw, y, x
-	(signed char) -1, +10*BLOW_UP, -1*BLOW_UP, // draw, y, x
+	(signed char) -1, +7*BLOW_UP, -1*BLOW_UP, // draw, y, x
 	(signed char) -1, +11*BLOW_UP, +5*BLOW_UP, // draw, y, x
 	(signed char) +2 // endmarker 
 };
