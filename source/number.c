3ba4d86990bb79c4162ad538669f6e98e4d9e088
phoboz <phoboz@vmx.nu>
2020-01-03 20:01:06 +0100

    Fixed vectorlists for numbers

--- source/number.c
+++ source/number.c
@@ -9,8 +9,8 @@
 
 #define BLOW_UP NUMBER_SCALE
 
-const signed char number_0[]=
-{	(signed char) +1, +6*BLOW_UP, +4*BLOW_UP, // sync and move to y, x
+const signed char number_250[]=
+{	(signed char) +0, +6*BLOW_UP, +4*BLOW_UP, // sync and move to y, x
 	(signed char) +0, +5*BLOW_UP, +0*BLOW_UP, // additional sync move to y, x
 	(signed char) -1, -6*BLOW_UP, +0*BLOW_UP, // draw, y, x
 	(signed char) -1, +0*BLOW_UP, +4*BLOW_UP, // draw, y, x
@@ -30,8 +30,8 @@ const signed char number_0[]=
 	(signed char) -1, +0*BLOW_UP, -4*BLOW_UP, // draw, y, x
 	(signed char) +2 // endmarker 
 };
-const signed char number_1[]=
-{	(signed char) +1, +6*BLOW_UP, +4*BLOW_UP, // sync and move to y, x
+const signed char number_500[]=
+{	(signed char) +0, +6*BLOW_UP, +4*BLOW_UP, // sync and move to y, x
 	(signed char) +0, +5*BLOW_UP, +0*BLOW_UP, // additional sync move to y, x
 	(signed char) -1, -6*BLOW_UP, +0*BLOW_UP, // draw, y, x
 	(signed char) -1, +0*BLOW_UP, +4*BLOW_UP, // draw, y, x
@@ -50,8 +50,8 @@ const signed char number_1[]=
 	(signed char) -1, +0*BLOW_UP, -4*BLOW_UP, // draw, y, x
 	(signed char) +2 // endmarker 
 };
-const signed char number_2[]=
-{	(signed char) +1, +6*BLOW_UP, +4*BLOW_UP, // sync and move to y, x
+const signed char number_750[]=
+{	(signed char) +0, +6*BLOW_UP, +4*BLOW_UP, // sync and move to y, x
 	(signed char) +0, +5*BLOW_UP, +0*BLOW_UP, // additional sync move to y, x
 	(signed char) -1, -6*BLOW_UP, +0*BLOW_UP, // draw, y, x
 	(signed char) -1, +0*BLOW_UP, +4*BLOW_UP, // draw, y, x
@@ -68,8 +68,8 @@ const signed char number_2[]=
 	(signed char) -1, +0*BLOW_UP, -4*BLOW_UP, // draw, y, x
 	(signed char) +2 // endmarker 
 };
-const signed char number_3[]=
-{	(signed char) +1, +6*BLOW_UP, +5*BLOW_UP, // sync and move to y, x
+const signed char number_1000[]=
+{	(signed char) +0, +6*BLOW_UP, +5*BLOW_UP, // sync and move to y, x
 	(signed char) +0, +5*BLOW_UP, +0*BLOW_UP, // additional sync move to y, x
 	(signed char) -1, -6*BLOW_UP, +0*BLOW_UP, // draw, y, x
 	(signed char) -1, +0*BLOW_UP, +4*BLOW_UP, // draw, y, x
@@ -90,10 +90,10 @@ const signed char number_3[]=
 	(signed char) +2 // endmarker 
 };
 const signed char* const number[]=
-{	 number_0, // list of all single vectorlists in this
-	 number_1,
-	 number_2,
-	 number_3,
+{	 number_250, // list of all single vectorlists in this
+	 number_500,
+	 number_750,
+	 number_1000,
 };
 
 // ***************************************************************************
--- source/number.h
+++ source/number.h
@@ -1,7 +1,12 @@
 #ifndef _NUMBER_H
 #define _NUMBER_H
 
-#define NUMBER_SCALE 8
+#define NUMBER_SCALE	8
+
+#define NUMBER_250		0
+#define NUMBER_500		1
+#define NUMBER_750		2
+#define NUMBER_1000	3
 
 extern const signed char* const number[];
 
